42d491d421e062dd6cbb66652f3eb91589910fe5
Karthik Prabhu <karthikprabhu22@gmail.com>
2020-04-15 21:16:09 -0700

    Addressed formatting issues

--- run_rule110.py
+++ run_rule110.py
@@ -3,7 +3,7 @@ import three_state_ca
 
 rule = 110
 length = 5
-initial_conditions = [1,0,1,0,1]
+initial_conditions = [1, 0, 1, 0, 1]
 field = three_state_ca.spacetime_field(rule, initial_conditions, length)
 three_state_ca.spacetime_diagram(field)
-plt.savefig('rule110.pdf')
+plt.savefig("rule110.pdf")
